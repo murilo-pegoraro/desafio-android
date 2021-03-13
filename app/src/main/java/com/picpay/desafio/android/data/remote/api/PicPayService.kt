3bc536feae602f7fe20a060d045e2637495805fd
Murilo Pegoraro <murilo.pegoraro@gmail.com>
2021-03-13 20:51:21 -0300

    removed legacy code

--- app/src/main/java/com/picpay/desafio/android/data/remote/api/PicPayService.kt
+++ app/src/main/java/com/picpay/desafio/android/data/remote/api/PicPayService.kt
@@ -2,14 +2,10 @@ package com.picpay.desafio.android.data.remote.api
 
 import com.picpay.desafio.android.data.remote.model.UserApi
 import io.reactivex.Single
-import retrofit2.Call
 import retrofit2.http.GET
 
 
 interface PicPayService {
-
-    @GET("users")
-    fun getUsers_OLD(): Call<List<UserApi>>
 
     @GET("users")
     fun getUsers(): Single<List<UserApi>>
